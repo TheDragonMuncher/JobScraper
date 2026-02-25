a2ff85cf82e09331b6cdc5732b6ac31fd4712e20
Matthew Eastwood <ilovelego@outlook.com>
2026-02-25 14:06:10 -0500

    create job posting model

--- JobScraper/Models/JobPosting.cs
+++ JobScraper/Models/JobPosting.cs
@@ -1,2 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace JobScraper.Models;
 
+public class JobPosting
+{
+    public int Id {get;set;}
+    public string SourceId {get;set;} = string.Empty;
+    public string Title {get;set;} = string.Empty;
+    public string Company {get;set;} = string.Empty;
+    public string Location {get;set;} = string.Empty;
+    public string Salary {get;set;} = string.Empty;
+    public string Description {get;set;} = string.Empty;
+    public DateTime DatePosted {get;set;}
+    public string Url {get;set;} = string.Empty;
+    public DateTime ScrapedAt {get;set;} = DateTime.UtcNow;
+}
